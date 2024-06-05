8e52fee3d35d78f0f8e3c927ef661e82feba062a
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-06-06 03:12:13 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -2,12 +2,18 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 await import("./src/env.js");
+
+if (process.env.NODE_ENV === "development") {
+	setupDevPlatform();
+}
 
 /** @type {import("next").NextConfig} */
 const config = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+	eslint: { ignoreDuringBuilds: true },
+	typescript: { ignoreBuildErrors: true },
 };
 
 export default config;
