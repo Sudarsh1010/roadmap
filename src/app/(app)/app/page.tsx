2e3762762f50739e608289d193c5dcb0696b8d0b
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-06-06 02:20:20 +0530

    removed db instance(drizzle) from trpc context

--- src/app/(app)/app/page.tsx
+++ src/app/(app)/app/page.tsx
@@ -1,5 +1,7 @@
 import { NoRoadmapPlaceholder } from "~/components/app/no-roadmap";
 
+export const runtime = "edge";
+
 export default async function Page() {
   return <NoRoadmapPlaceholder />;
 }
